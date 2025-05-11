64049aa6fd21c125b3ca7c66b8dd4be43ed1baf3
Abed Bashir <56051101+AbedBashir@users.noreply.github.com>
2025-05-11 13:31:28 +0300

    Fixed scrolling images

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,95 +1,51 @@
 import sohati from "../components/assets/sohati.png";
-import sohati__scroll from "../components/assets/sohati__scroll.png";
-import oxfordscholastica from "../components/assets/oxfordscholastica.png";
-import oxfordscholastica__scroll from "../components/assets/oxfordscholastica__scroll.png";
-import juniorkids from "../components/assets/juniorkids.png";
-import juniorkids__scroll from "../components/assets/juniorkids__scroll.png";
-import lesfinfinettes from "../components/assets/lesfinfinettes.png";
-import lesfinfinettes__scroll from "../components/assets/lesfinfinettes__scroll.png";
-import chapterone from "../components/assets/chapterone.png";
-import chapterone__scroll from "../components/assets/chapterone__scroll.png";
-import mercermackay from "../components/assets/mercermackay.png";
-import mercermackay__scroll from "../components/assets/mercermackay__scroll.png";
-import caesarsflame from "../components/assets/caesarsflame.png";
-//  import caesarsflame__scroll from "../components/assets/caesarsflame__scroll.png";
-//  import wastedtreasures__scroll from "../components/assets/wastedtreasures__scroll.png";
-import loolia from "../components/assets/loolia.png";
-import loolia__scroll from "../components/assets/loolia__scroll.png";
-import silvascience from "../components/assets/silvascience.png";
-import silvascience__scroll from "../components/assets/silvascience__scroll.png";
-import naturesage from "../components/assets/naturesage.png";
-import naturesage__scroll from "../components/assets/naturesage__scroll.png";
-import lestudiono20 from "../components/assets/lestudiono20.png";
-import lestudiono20__scroll from "../components/assets/lestudiono20__scroll.png";
-import jeuxmania from "../components/assets/jeuxmania.png";
-import jeuxmania__scroll from "../components/assets/jeuxmania__scroll.png";
-import go360 from "../components/assets/go360.png";
-import go360__scroll from "../components/assets/go360__scroll.png";
-import verb from "../components/assets/verb.png";
-import verb__scroll from "../components/assets/verb__scroll.png";
-import authenticsecurite from "../components/assets/authenticsecurite.png";
-import authenticsecurite__scroll from "../components/assets/authenticsecurite__scroll.png";
-import noahbissi from "../components/assets/noahbissi.png";
-import noahbissi__scroll from "../components/assets/noahbissi__scroll.png";
-import robertalexander from "../components/assets/robertalexander.png";
-import robertalexander__scroll from "../components/assets/robertalexander__scroll.png";
-import kosy from "../components/assets/kosy.png";
-import kosy__scroll from "../components/assets/kosy__scroll.png";
-import riieq from "../components/assets/riieq.png";
-import riieq__scroll from "../components/assets/riieq__scroll.png";
-import pineconerow from "../components/assets/pineconerow.png";
-import pineconerow__scroll from "../components/assets/pineconerow__scroll.png";
-import bulletvpn from "../components/assets/bulletvpn.png";
-import bulletvpn__scroll from "../components/assets/bulletvpn__scroll.png";
-import marklumber from "../components/assets/marklumber.png";
-import marklumber__scroll from "../components/assets/marklumber__scroll.png";
-import flancnord from "../components/assets/flancnord.png";
-import flancnord__scroll from "../components/assets/flancnord__scroll.png";
-import boutiquesp from "../components/assets/boutiquesp.png";
-import boutiquesp__scroll from "../components/assets/boutiquesp__scroll.png";
-import greenmountrecycling from "../components/assets/greenmountrecycling.png";
-import greenmountrecycling__scroll from "../components/assets/greenmountrecycling__scroll.png";
-import verslavenir from "../components/assets/verslavenir.png";
-import verslavenir__scroll from "../components/assets/verslavenir__scroll.png";
-import wastedtreasures from "../components/assets/wastedtreasures.png";
-import bimoo from "../components/assets/bimoo.png";
-import bimoo__scroll from "../components/assets/bimoo__scroll.png";
-import learnopolis from "../components/assets/learnopolis.png";
-import learnopolis__scroll from "../components/assets/learnopolis__scroll.png";
-import partyomax from "../components/assets/partyomax.png";
-import partyomax__scroll from "../components/assets/partyomax__scroll.png";
-import planb from "../components/assets/planb.png";
-import planb__scroll from "../components/assets/planb__scroll.png";
-import vmania from "../components/assets/vmania.png";
-import vmania__scroll from "../components/assets/vmania__scroll.png";
-import e3electricite from "../components/assets/e3electricite.png";
-import e3electricite__scroll from "../components/assets/e3electricite__scroll.png";
-import vignoble from "../components/assets/vignoble.png";
-import vignoble__scroll from "../components/assets/vignoble__scroll.png";
-import filco from "../components/assets/filco.png";
-import filco__scroll from "../components/assets/filco__scroll.png";
-import cliniqueodyssey from "../components/assets/cliniqueodyssey.png";
-import cliniqueodyssey__scroll from "../components/assets/cliniqueodyssey__scroll.png";
-import sislebanon from "../components/assets/sislebanon.png";
-import sislebanon__scroll from "../components/assets/sislebanon__scroll.png";
-import capsulcase from "../components/assets/capsulcase.png";
-import capsulcase__scroll from "../components/assets/capsulcase__scroll.png";
-import khatiblawfirm from "../components/assets/khatiblawfirm.png";
-import khatiblawfirm__scroll from "../components/assets/khatiblawfirm__scroll.png";
-import gayane from "../components/assets/gayane.png";
-import gayane__scroll from "../components/assets/gayane__scroll.png";
-import uniquecoaching from "../components/assets/uniquecoaching.png";
-import uniquecoaching__scroll from "../components/assets/uniquecoaching__scroll.png";
-import cmeg from "../components/assets/cmeg.png";
-import cmeg__scroll from "../components/assets/cmeg__scroll.png";
-import willgo from "../components/assets/willgo.png";
-import willgo__scroll from "../components/assets/willgo__scroll.png";
+import oxfordscholastica from "../../assets/oxfordscholastica.png";
+import juniorkids from "../../assets/juniorkids.png";
+import lesfinfinettes from "../../assets/lesfinfinettes.png";
+import chapterone from "../../assets/chapterone.png";
+import mercermackay from "../../assets/mercermackay.png";
+import caesarsflame from "../../assets/caesarsflame.png";
+import loolia from "../../assets/loolia.png";
+import silvascience from "../../assets/silvascience.png";
+import naturesage from "../../assets/naturesage.png";
+import lestudiono20 from "../../assets/lestudiono20.png";
+import jeuxmania from "../../assets/jeuxmania.png";
+import go360 from "../../assets/go360.png";
+import verb from "../../assets/verb.png";
+import authenticsecurite from "../../assets/authenticsecurite.png";
+import noahbissi from "../../assets/noahbissi.png";
+import robertalexander from "../../assets/robertalexander.png";
+import kosy from "../../assets/kosy.png";
+import riieq from "../../assets/riieq.png";
+import pineconerow from "../../assets/pineconerow.png";
+import bulletvpn from "../../assets/bulletvpn.png";
+import marklumber from "../../assets/marklumber.png";
+import flancnord from "../../assets/flancnord.png";
+import boutiquesp from "../../assets/boutiquesp.png";
+import greenmountrecycling from "../../assets/greenmountrecycling.png";
+import verslavenir from "../../assets/verslavenir.png";
+import wastedtreasures from "../../assets/wastedtreasures.png";
+import bimoo from "../../assets/bimoo.png";
+import learnopolis from "../../assets/learnopolis.png";
+import partyomax from "../../assets/partyomax.png";
+import planb from "../../assets/planb.png";
+import vmania from "../../assets/vmania.png";
+import e3electricite from "../../assets/e3electricite.png";
+import vignoble from "../../assets/vignoble.png";
+import filco from "../../assets/filco.png";
+import cliniqueodyssey from "../../assets/cliniqueodyssey.png";
+import sislebanon from "../../assets/sislebanon.png";
+import capsulcase from "../../assets/capsulcase.png";
+import khatiblawfirm from "../../assets/khatiblawfirm.png";
+import gayane from "../../assets/gayane.png";
+import uniquecoaching from "../../assets/uniquecoaching.png";
+import cmeg from "../../assets/cmeg.png";
+import willgo from "../../assets/willgo.png";
 
 export const projectsData = [
   {
     id: 1,
     image: sohati,
-    scrolling_image: sohati__scroll,
     title: "SohatiCare",
     category: "shopify",
     company: "Parallel Holdings",
@@ -99,7 +55,6 @@ export const projectsData = [
   {
     id: 2,
     image: oxfordscholastica,
-    scrolling_image: oxfordscholastica__scroll,
     title: "Oxford Scholastica",
     category: "wordpress",
     company: "Growth Hacker",
@@ -109,7 +64,6 @@ export const projectsData = [
   {
     id: 3,
     image: juniorkids,
-    scrolling_image: juniorkids__scroll,
     title: "Junior Kids",
     category: "shopify",
     company: "Growth Hacker",
@@ -119,7 +73,6 @@ export const projectsData = [
   {
     id: 4,
     image: lesfinfinettes,
-    scrolling_image: lesfinfinettes__scroll,
     title: "Les FinFinettes",
     category: "wordpress",
     company: "Growth Hacker",
@@ -129,7 +82,6 @@ export const projectsData = [
   {
     id: 5,
     image: chapterone,
-    scrolling_image: chapterone__scroll,
     title: "Chapter One",
     category: "shopify",
     company: "BashFusion",
@@ -139,7 +91,6 @@ export const projectsData = [
   {
     id: 6,
     image: mercermackay,
-    scrolling_image: mercermackay__scroll,
     title: "Mercer Mackay",
     category: "wordpress",
     company: "Growth Hacker",
@@ -149,7 +100,6 @@ export const projectsData = [
   {
     id: 7,
     image: caesarsflame,
-    // scrolling_image: caesarsflame__scroll,
     title: "Caesars Flame",
     category: "wordpress",
     company: "BashFusion",
@@ -159,7 +109,6 @@ export const projectsData = [
   {
     id: 8,
     image: loolia,
-    scrolling_image: loolia__scroll,
     title: "LooliaCloset",
     category: "shopify",
     company: "Parallel Holdings",
@@ -169,7 +118,6 @@ export const projectsData = [
   {
     id: 9,
     image: silvascience,
-    scrolling_image: silvascience__scroll,
     title: "Silva Science",
     category: "wordpress",
     company: "Growth Hacker",
@@ -179,7 +127,6 @@ export const projectsData = [
   {
     id: 10,
     image: naturesage,
-    scrolling_image: naturesage__scroll,
     title: "Naturesage",
     category: "shopify",
     company: "Growth Hacker",
@@ -189,7 +136,6 @@ export const projectsData = [
   {
     id: 11,
     image: lestudiono20,
-    scrolling_image: lestudiono20__scroll,
     title: "Le Studio N.20",
     category: "wordpress",
     company: "Growth Hacker",
@@ -199,7 +145,6 @@ export const projectsData = [
   {
     id: 12,
     image: jeuxmania,
-    scrolling_image: jeuxmania__scroll,
     title: "Jeux Mania",
     category: "shopify",
     company: "Growth Hacker",
@@ -209,7 +154,6 @@ export const projectsData = [
   {
     id: 13,
     image: go360,
-    scrolling_image: go360__scroll,
     title: "Go360",
     category: "wordpress",
     company: "Growth Hacker",
@@ -219,7 +163,6 @@ export const projectsData = [
   {
     id: 14,
     image: verb,
-    scrolling_image: verb__scroll,
     title: "Verb Products",
     category: "shopify",
     company: "Growth Hacker",
@@ -229,7 +172,6 @@ export const projectsData = [
   {
     id: 15,
     image: authenticsecurite,
-    scrolling_image: authenticsecurite__scroll,
     title: "Authentik Securite",
     category: "wordpress",
     company: "Growth Hacker",
@@ -239,7 +181,6 @@ export const projectsData = [
   {
     id: 16,
     image: noahbissi,
-    scrolling_image: noahbissi__scroll,
     title: "Noah Bissi",
     category: "shopify",
     company: "Growth Hacker",
@@ -249,7 +190,6 @@ export const projectsData = [
   {
     id: 17,
     image: robertalexander,
-    scrolling_image: robertalexander__scroll,
     title: "Robert Alexander Salon",
     category: "wordpress",
     company: "BashFusion",
@@ -259,7 +199,6 @@ export const projectsData = [
   {
     id: 18,
     image: kosy,
-    scrolling_image: kosy__scroll,
     title: "Simplement Kosy",
     category: "shopify",
     company: "Growth Hacker",
@@ -269,7 +208,6 @@ export const projectsData = [
   {
     id: 19,
     image: riieq,
-    scrolling_image: riieq__scroll,
     title: "RIIEQ",
     category: "wordpress",
     company: "Growth Hacker",
@@ -279,7 +217,6 @@ export const projectsData = [
   {
     id: 20,
     image: pineconerow,
-    scrolling_image: pineconerow__scroll,
     title: "Pinecone Row",
     category: "shopify",
     company: "BashFusion",
@@ -289,7 +226,6 @@ export const projectsData = [
   {
     id: 21,
     image: bulletvpn,
-    scrolling_image: bulletvpn__scroll,
     title: "BulletVPN",
     category: "wordpress",
     company: "Securealm",
@@ -299,7 +235,6 @@ export const projectsData = [
   {
     id: 22,
     image: marklumber,
-    scrolling_image: marklumber__scroll,
     title: "Mark Lumber",
     category: "shopify",
     company: "Growth Hacker",
@@ -309,7 +244,6 @@ export const projectsData = [
   {
     id: 23,
     image: flancnord,
-    scrolling_image: flancnord__scroll,
     title: "Verger Du Flanc Nord",
     category: "wordpress",
     company: "Growth Hacker",
@@ -319,7 +253,6 @@ export const projectsData = [
   {
     id: 24,
     image: boutiquesp,
-    scrolling_image: boutiquesp__scroll,
     title: "Boutique SP",
     category: "shopify",
     company: "Growth Hacker",
@@ -329,7 +262,6 @@ export const projectsData = [
   {
     id: 25,
     image: greenmountrecycling,
-    scrolling_image: greenmountrecycling__scroll,
     title: "Green Mount Recycling",
     category: "wordpress",
     company: "BashFusion",
@@ -339,7 +271,6 @@ export const projectsData = [
   {
     id: 26,
     image: verslavenir,
-    scrolling_image: verslavenir__scroll,
     title: "Vers L'avenir",
     category: "shopify",
     company: "Growth Hacker",
@@ -349,7 +280,6 @@ export const projectsData = [
   {
     id: 27,
     image: wastedtreasures,
-    // scrolling_image: wastedtreasures__scroll,
     title: "Wasted Treasures",
     category: "wordpress",
     company: "BashFusion",
@@ -359,7 +289,6 @@ export const projectsData = [
   {
     id: 28,
     image: bimoo,
-    scrolling_image: bimoo__scroll,
     title: "BimOo",
     category: "shopify",
     company: "Growth Hacker",
@@ -369,7 +298,6 @@ export const projectsData = [
   {
     id: 29,
     image: learnopolis,
-    scrolling_image: learnopolis__scroll,
     title: "Learnopolis",
     category: "wordpress",
     company: "Growth Hacker",
@@ -379,7 +307,6 @@ export const projectsData = [
   {
     id: 30,
     image: partyomax,
-    scrolling_image: partyomax__scroll,
     title: "Party Ô Max",
     category: "shopify",
     company: "Growth Hacker",
@@ -389,7 +316,6 @@ export const projectsData = [
   {
     id: 31,
     image: planb,
-    scrolling_image: planb__scroll,
     title: "Plan B Concept",
     category: "wordpress",
     company: "BashFusion",
@@ -399,7 +325,6 @@ export const projectsData = [
   {
     id: 32,
     image: vmania,
-    scrolling_image: vmania__scroll,
     title: "V-Mania",
     category: "shopify",
     company: "Growth Hacker",
@@ -409,7 +334,6 @@ export const projectsData = [
   {
     id: 33,
     image: e3electricite,
-    scrolling_image: e3electricite__scroll,
     title: "E3 Electricite",
     category: "wordpress",
     company: "Growth Hacker",
@@ -419,7 +343,6 @@ export const projectsData = [
   {
     id: 34,
     image: vignoble,
-    scrolling_image: vignoble__scroll,
     title: "Vignoble Des Coteaux Artisans",
     category: "shopify",
     company: "Growth Hacker",
@@ -429,7 +352,6 @@ export const projectsData = [
   {
     id: 35,
     image: filco,
-    scrolling_image: filco__scroll,
     title: "Filco LB",
     category: "wordpress",
     company: "BashFusion",
@@ -439,7 +361,6 @@ export const projectsData = [
   {
     id: 36,
     image: cliniqueodyssey,
-    scrolling_image: cliniqueodyssey__scroll,
     title: "Clinique Odyssey",
     category: "shopify",
     company: "Growth Hacker",
@@ -449,8 +370,6 @@ export const projectsData = [
   {
     id: 37,
     image: sislebanon,
-    scrolling_image: sislebanon__scroll,
-    title: "SIS Lebanon",
     category: "wordpress",
     company: "Growth Hacker",
     location: "CAD",
@@ -459,8 +378,6 @@ export const projectsData = [
   {
     id: 38,
     image: capsulcase,
-    scrolling_image: capsulcase__scroll,
-    title: "Capsul Case",
     category: "shopify",
     company: "Growth Hacker",
     location: "CAD",
@@ -469,8 +386,6 @@ export const projectsData = [
   {
     id: 39,
     image: khatiblawfirm,
-    scrolling_image: khatiblawfirm__scroll,
-    title: "El-Khatib Law Firm",
     category: "wordpress",
     company: "BashFusion",
     location: "LBN",
@@ -479,7 +394,6 @@ export const projectsData = [
   {
     id: 40,
     image: gayane,
-    scrolling_image: gayane__scroll,
     title: "Gayane Avertisyan",
     category: "shopify",
     company: "Growth Hacker",
@@ -489,7 +403,6 @@ export const projectsData = [
   {
     id: 41,
     image: uniquecoaching,
-    scrolling_image: uniquecoaching__scroll,
     title: "Unique Coaching",
     category: "wordpress",
     company: "Growth Hacker",
@@ -499,7 +412,6 @@ export const projectsData = [
   {
     id: 42,
     image: cmeg,
-    scrolling_image: cmeg__scroll,
     title: "Centre Du Moteur Granby",
     category: "shopify",
     company: "Growth Hacker",
@@ -509,7 +421,6 @@ export const projectsData = [
   {
     id: 43,
     image: willgo,
-    scrolling_image: willgo__scroll,
     title: "Déménagement Will Go",
     category: "wordpress",
     company: "Growth Hacker",
