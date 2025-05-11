88c95932d51f38ac558edc3816a49b2ff23bfb43
Abed Bashir <56051101+AbedBashir@users.noreply.github.com>
2025-05-11 13:51:30 +0300

    Fixing the about image

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -109,7 +109,7 @@ export const About: React.FC = () => {
               <div className="absolute inset-0 bg-primary-500/20 rounded-md transform translate-x-4 translate-y-4"></div>
               <div className="relative bg-slate-100 dark:bg-navy-light rounded-md overflow-hidden border-2 border-primary-500/30 p-2">
                 <img
-                  src="src/components/assets/about.jpg"
+                  src="AB-Portfolio/src/components/assets/about.png"
                   alt="Profile"
                   className="w-full h-auto rounded-md grayscale hover:grayscale-0 transition-all duration-300"
                 />
